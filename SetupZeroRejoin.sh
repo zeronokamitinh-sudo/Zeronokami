15b9f3f8c83e4298629046c8ee89ab8433450d00
zeronokamitinh-sudo <zeronokami.tinh@gmail.com>
2026-01-31 09:53:14 +0700

    SetupZeroRejoin.sh

--- SetupZeroRejoin.sh
+++ SetupZeroRejoin.sh
@@ -12,6 +12,6 @@ pip install requests urllib3 colorama
 
 echo ""
 echo "Setup xong! Bây giờ chạy lệnh sau để start tool:"
-echo "su -c \"export PATH=\$PATH:/data/data/com.termux/files/usr/bin && export TERM=xterm-256color && python <(curl -s https://raw.githubusercontent.com/YOURUSERNAME/ToolRejoin/main/zero_rejoin.py)\""
+echo "su -c \"export PATH=\$PATH:/data/data/com.termux/files/usr/bin && export TERM=xterm-256color && python <(curl -s https://raw.githubusercontent.com/Zeronokami/ToolRejoin/main/zero_rejoin.py)\""
 echo ""
 echo "Hoặc vào thư mục Download rồi chạy python zero_rejoin.py"
